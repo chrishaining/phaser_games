e744c2f789228689a4956aa68fa1c6ecb6110676
Chris <chrishaining@yahoo.co.uk>
2020-01-04 21:29:02 +0000

    deletes comments

--- game.js
+++ game.js
@@ -1,12 +1,10 @@
 const gameState = {}
 
 function preload() {
-    // this.load.image('codey', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/codey.png');
     this.load.image('friend', '/assets/frog.png');
   }
   
 function create() {
-  // gameState.codey = this.add.sprite(100, 100, 'codey')
   gameState.friend = this.add.sprite(200, 200, 'friend')
   gameState.cursors = this.input.keyboard.createCursorKeys();
 }
