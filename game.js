67a65da3607b9c6bd05950e02b5a6356d25c9903
Chris <chrishaining@yahoo.co.uk>
2020-01-05 21:20:29 +0000

    changes name of sprite

--- game.js
+++ game.js
@@ -1,27 +1,27 @@
 const gameState = {}
 
 function preload() {
-    this.load.image('friend', 'assets/frog.png');
+    this.load.image('frog', 'assets/frog.png');
     this.load.image('floor', 'assets/floor.png');
     this.load.image('star', 'assets/star.png')
     this.load.audio('croak', 'assets/frogsound.mp3');
   }
   
 function create() {
-  gameState.friend = this.physics.add.sprite(200, 400, 'friend')
+  gameState.frog = this.physics.add.sprite(200, 400, 'frog')
   gameState.croak = this.sound.add('croak')
   gameState.cursors = this.input.keyboard.createCursorKeys();
   const floor = this.physics.add.staticGroup();
   floor.create(225, 510, 'floor');
-  gameState.friend.setCollideWorldBounds(true);
-  this.physics.add.collider(gameState.friend, floor)
+  gameState.frog.setCollideWorldBounds(true);
+  this.physics.add.collider(gameState.frog, floor)
 
   const star = this.physics.add.group();
   function makeStar() {
     let xCoord = Math.random() * 450;
     star.create(xCoord, 10, 'star');
   }
-  makeStar();
+  // makeStar();
 
   const makeStarLoop = this.time.addEvent({
     callback: makeStar,
@@ -34,20 +34,20 @@ function create() {
   
 function update() {
     if (gameState.cursors.down.isDown) {
-        gameState.friend.y += 1;
+        gameState.frog.y += 1;
         gameState.croak.play();
 
       }
     if (gameState.cursors.up.isDown) {
-        gameState.friend.y -= 1;
+        gameState.frog.y -= 1;
         gameState.croak.play();
       }
     if (gameState.cursors.left.isDown) {
-        gameState.friend.x -= 1;
+        gameState.frog.x -= 1;
         gameState.croak.play();
       }
     if (gameState.cursors.right.isDown) {
-        gameState.friend.x += 1;
+        gameState.frog.x += 1;
         gameState.croak.play();
       }
 }
